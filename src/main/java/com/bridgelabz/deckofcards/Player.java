ac5b1f7c8c1e3d67daa01390a0f112cf165aee92
Subhamdas20 <subham21.sd@gmail.com>
2021-11-13 20:43:37 +0530

    Added : uc3 to sequence players

--- src/main/java/com/bridgelabz/deckofcards/Player.java
+++ src/main/java/com/bridgelabz/deckofcards/Player.java
@@ -1,9 +1,22 @@
 package com.bridgelabz.deckofcards;
 
+import java.util.ArrayList;
+
 public class Player {
+    ArrayList<String> cardList = new ArrayList<>();
     public Player(String firstname) {
         this.firstname = firstname;
     }
 
     String firstname;
+
+    public void setCardList(String card){
+        cardList.add(card);
+    }
+    public void getCardList(){
+        //get all cards inside cardList
+        for(String position : cardList){
+            System.out.print(position+", ");
+        }
+    }
 }
